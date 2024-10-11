30278618171762913c4cf911cafcd04382382519
Ryan Atkinson <64192853+Ryan-Atkinson87@users.noreply.github.com>
2024-10-11 17:35:55 +0100

    cleaned up the create-react-app install

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Welcome to my Raspberry Pi project page!
+        </h1>
+    </div>
+  );
+}
+
+export default App;
